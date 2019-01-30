ba5aa0433da7d2af984ee20d598a3a4752f483ed
Pascal <Pascal@10.143.62.185>
2019-01-30 15:25:48 -0600

    Sprites added

--- com.CH.Iris/src/game/Assets.java
+++ com.CH.Iris/src/game/Assets.java
@@ -1,9 +1,32 @@
 package game;
 
+import gfx.Sprite;
+import gfx.SpriteSheet;
+
 public class Assets {
 
+	private static final String ENTITY = "/entity/";
+	private static final String WORLD = "/world/";
+	
+	private static SpriteSheet s_tile;
+	private static SpriteSheet s_ship;
+	private static SpriteSheet s_asteriod;
+	
+	public static Sprite blackTile;
+	public static Sprite starTile;
+	public static Sprite bigStarTile;
+	public static Sprite manyStarTile;
+	public static Sprite ship;
+	public static Sprite asteroid;
+	
 	public static void initialize() {
+		s_tile = new SpriteSheet(WORLD + "tile.png");
+		s_ship = new SpriteSheet(ENTITY + "ship.png");
+		s_asteriod = new SpriteSheet(ENTITY + "asteriod.png");
+		
+		
 		
 	}
+	
 	
 }
--- com.CH.Iris/src/gfx/Sprite.java
+++ com.CH.Iris/src/gfx/Sprite.java
@@ -1,13 +1,26 @@
 package gfx;
 
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+
+import game.Game;
 
 public class Sprite {
 
 	BufferedImage sprite;
 	
 	public Sprite(int x, int y, int size, SpriteSheet sheet) {
-		sheet.cut(x, y, size); 
+		BufferedImage holding = sheet.cut(x, y, size); 
+		
+		sprite = new BufferedImage(size * Game.GAMESCALE, size * Game.GAMESCALE, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = sprite.getGraphics();
+		g.drawImage(holding, 0, 0, size * Game.GAMESCALE, size * Game.GAMESCALE, null);
+		g.dispose();
+		
+	}
+	
+	public void render(int x, int y, Graphics g) {
+		g.drawImage(sprite, x, y, null);
 	}
 	
 	
