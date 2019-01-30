c8ba7923a0da18941b96fc442524400181125c82
Connor <Connor@10.139.99.102>
2019-01-30 14:59:04 -0600

    spite

--- com.CH.Iris/src/entites/Entity.java
+++ com.CH.Iris/src/entites/Entity.java
@@ -1,12 +1,15 @@
 package entites;
 
 import runtime.Handler;
+import gfx.Sprite;
 
 //bitc hass
 public abstract class Entity {
     protected Handler theHandler;
     protected int xPos;
     protected int yPos;
-    //protected Sprite sprite;
+    protected Sprite sprite;
+    
+    
     
 }
