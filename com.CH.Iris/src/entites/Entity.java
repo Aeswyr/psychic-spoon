4bd7a9af714cbcda33d7462706b3c06e667e8a9b
Connor <Connor@10.139.99.102>
2019-01-30 15:01:46 -0600

    Entity class

--- com.CH.Iris/src/entites/Entity.java
+++ com.CH.Iris/src/entites/Entity.java
@@ -1,15 +1,15 @@
 package entites;
 
 import runtime.Handler;
+import java.awt.Graphics;
 import gfx.Sprite;
 
-//bitc hass
 public abstract class Entity {
     protected Handler theHandler;
     protected int xPos;
     protected int yPos;
-    protected Sprite sprite;
-    
-    
-    
+    protected Sprite sprite; 
+
+    public abstract void update();
+    public abstract void render(Graphics g);
 }
