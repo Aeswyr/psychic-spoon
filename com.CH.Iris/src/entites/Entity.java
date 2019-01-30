8f1c7bd4879ceeb9d38070f63af018f42d9393bd
Connor <Connor@10.139.99.102>
2019-01-30 14:48:52 -0600

    Started Entity

---
+++ com.CH.Iris/src/entites/Entity.java
@@ -0,0 +1,12 @@
+package entites;
+
+import runtime.Handler;
+
+
+public abstract class Entity {
+    protected Handler theHandler;
+    protected int xPos;
+    protected int yPos;
+    //protected Sprite sprite;
+    
+}
--- com.CH.Iris/src/game/Initialize.java
+++ com.CH.Iris/src/game/Initialize.java
@@ -4,7 +4,7 @@ public class Initialize {
 
 	public static void main(String[] args) {
 		Game game = new Game();
-		game.start();
+		game.start(); 
 	}
 	
 }
