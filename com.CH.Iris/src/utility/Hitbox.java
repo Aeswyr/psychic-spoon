52b2bceab92b0d69afa9ac9ea47e8aac6c4af088
dillo <dillo@10.143.51.43>
2019-01-30 16:48:15 -0600

    Updated Hitbox

--- com.CH.Iris/src/utility/Hitbox.java
+++ com.CH.Iris/src/utility/Hitbox.java
@@ -1,5 +1,64 @@
 package utility;
 
-public class Hitbox {
 
+import entities.Entity;
+
+
+public class Hitbox {
+	
+	// Height and width of current object
+	private int height;
+	private int width;
+	
+	// Height and width of another enity 
+	private int eHeight;
+	private int eWidth;
+	
+	// Offset to trim the hitbox of the current object
+	private int offsetX;
+	private int offsetY;
+	
+	// Offset to trim the hitbox of another entity 
+	private int eOffsetX;
+	private int eOffsetY;
+	
+	// Position of the current object
+	private int positionX;
+	private int positionY;
+	
+	// Entity being compared to the current object
+	private Entity e2;
+	
+	
+	public Hitbox(int height, int width) {
+		this.height = height;
+		this.width = width;
+	}	
+	
+	/*
+	 * Checks if any part of this entity is touching another entity
+	 * 
+	 * 
+	 * 
+	 */
+	public boolean contains(Entity e2) {
+		
+		int positionXR = positionX + width;
+		int positionYB = positionY + height;
+		eWidth = e2.getWidth();
+		eHeight = e2.getHeight();
+		
+		
+		if (positionX > e2.getXPos() && positionX < e2.getXPos() + eWidth) {
+			
+			if (positionY < e2.getYPos() && positionY > e2.getYPos() + eHeight) {
+				return true;
+			}
+			
+		}
+		
+		return false;
+				
+	}
+	
 }
