a3ffdae645b7d0e0d0aa2d60b9ed7a1cb134583d
Pascal <Pascal@10.143.62.185>
2019-01-30 19:05:36 -0600

    camera skeleton 2

---
+++ com.CH.Iris/src/runtime/Camera.java
@@ -0,0 +1,43 @@
+package runtime;
+
+import entities.Entity;
+import game.Screen;
+
+public class Camera {
+	
+
+	  private static final int WIDTH = (int) Screen.dim.getWidth();
+	  private static final int HEIGHT = (int) Screen.dim.getHeight();
+
+	  private int xOffset, yOffset;
+	  private Handler handler;
+
+	  public Camera(Handler handler) {
+	    this.xOffset = 0;
+	    this.yOffset = 0;
+
+	    this.handler = handler;
+	  }
+
+	  public void centerOnEntity(Entity e) {
+	    xOffset = e.getXPos() - WIDTH / 2;
+	    if (xOffset > handler.getWorld().getWidth() - WIDTH)
+	      xOffset = handler.getWorld().getWidth() - WIDTH;
+	    if (xOffset < 0)
+	      xOffset = 0;
+
+	    yOffset = e.getYPos() - HEIGHT / 2;
+	    if (yOffset > handler.getWorld().getHeight() - HEIGHT)
+	      yOffset = handler.getWorld().getHeight() - HEIGHT;
+	    if (yOffset < 0)
+	      yOffset = 0;
+	  }
+
+	  public int xOffset() {
+	    return xOffset;
+	  }
+
+	  public int yOffset() {
+	    return yOffset;
+	  }
+}
